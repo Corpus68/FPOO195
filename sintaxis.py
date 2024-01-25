2d0d544d0ac4ec9e6ccdff8a2f40041eae948419
Corpus68 <157434679+Corpus68@users.noreply.github.com>
2024-01-24 20:25:02 -0600

    Ya termine la practica

---
+++ sintaxis.py
@@ -0,0 +1,66 @@
+#soy un comentario de 1 linea
+'''soy un comentario
+de
+varias lineas
+'''
+'''
+# 2.Cadenas
+
+print('Soy una cadena')
+print("Soy otra cadena")
+
+a='Mis sagas favoritas de juegos son:\n'
+b='Assasins creed'
+c='Kingdom Hearts'
+print (a,b,c)
+#contar caracteres
+print(len(a))
+
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+'''
+
+#3 Variables
+'''
+x= int(5)
+y= str(4)
+z=float(3.2)
+
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+numero = random.randrange(1,15)
+print(numero)
+'''
+'''
+#4. Solicitud de datos
+
+var1=input("Introduce cualquier dato")
+
+var2=str(input("Introduce cadenas"))
+var3=int(input("Introduce solo enteros"))
+var4=float(input("Introduce numeros decimales"))
+
+print(var1, var2, var3, var4)
+'''
+ #5 Bolleans, operadores de comparacion y 
+'''
+print(10>9)
+print(10==9)
+print(10<9)
+print(10>=9)
+print(10!=9)
+print(10<=9)
+'''
+#Para opreaciones binarias
+x =1
+print(x < 5 & x < 10)
+print(x < 5 | x < 10)
+print(not(x < 5 & x < 10))
+
+
